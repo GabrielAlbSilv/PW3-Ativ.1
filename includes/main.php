c0f46cd4466a39b0d3c8fdf4111dde2d75c067d1
GabrielAlbSilv <gabrielalbuquerquemoura@gmail.com>
2022-03-22 20:12:40 -0300

    primeira commit

---
+++ includes/main.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade-Pw3</title>
+     <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<br>
+<br>
+ <section class=" grid">
+                <div class="item">
+<h2>Gerenciando a Hero Api</h2>
+
+  <form name="cadastro" method="post" action="Api.php">
+    Id: &nbsp; <input type="text" name="id" size="40" maxlength="20"> <br/> <br/>
+    Nome: <input type="text" name="name"> <br/> <br/>
+    Nome real: <input type="text" name="realname"> <br/> <br/>
+    Avaliação: <input type="text" name="rating"> <br/> <br/>
+    Equipe: <input type="text" name="teamaffiliation"> <br/> <br/>
+    <br/>
+    <input type="hidden" name="login" value="admin">
+    <br/>
+    &nbsp; &nbsp; &nbsp;
+    <input type="button" name="VOLTAR">
+    &nbsp; &nbsp; &nbsp;
+    <input type="reset" name="LIMPAR">
+    <input type="submit" name="ENVIAR">
+    <br/>
+    <br/>
+    <br/>
+    <a href="#" onclick="document.cadastro.reset()">LIMPAR</a>
+    
+  </form>
+  </div>
+  </section>
+</body>
+</html>
+
